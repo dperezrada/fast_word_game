4d6cd171cc6ca77e974d47dc8f04e19e07b2b666
Daniel Pérez Rada <dperezrada@gmail.com>
2012-04-28 13:07:29 -0300

    You can add users to the game Adding README

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-Game = module.exports  = function(){};
+var _ = require('underscore');
+
+Game = module.exports  = function(){
+	this.users = [];
+	var self = this;
+	return {
+		get_users: function(){
+			return self.users;
+		},
+		add_user: function(user){
+			self.users = _.union(self.users, [user])
+		}
+	};
+};
--- tests.js
+++ tests.js
@@ -5,4 +5,10 @@ module.exports = {
 		game = new Game();
 		assert.isNotNull(game);
 	},
+	'test Add users to the game': function(beforeExit, assert) {
+		game = new Game();
+		assert.eql([], game.get_users())
+		game.add_user({'name': 'daniel'})
+		assert.eql([{'name': 'daniel'}], game.get_users())
+	},
 }
