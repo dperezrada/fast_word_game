2a1120f742120d13cd4dea4c4cd592c18503f569
Daniel Pérez Rada <dperezrada@gmail.com>
2012-04-29 09:31:19 -0400

    FIX: some problems with socket.io (not using broadcast)

--- server.js
+++ server.js
@@ -12,11 +12,18 @@ game.set_word(get_word());
 io.sockets.on('connection', function (socket) {
 	socket.on('set_name', function (data) {
 		game.add_user(socket.id, data.name);
-		socket.emit('new_user', {  word: game.get_word(), users: game.get_users() });
+		socket.emit('welcome', {  word: game.get_word(), users: game.get_users(), points: game.get_scores() });
+		socket.broadcast.emit('new_user', {  users: game.get_users(), points: game.get_scores() });
 	});
 	socket.on('word_typed', function (data) {
 		if(game.check_winner(socket.id, data.word)){
-			socket.emit('winner', { user: socket.id, points: game.get_scores(), word: get_word()});
+			new_word = get_word();
+			game.set_word(new_word);
+			socket.emit('winner', { user: socket.id, points: game.get_scores(), word: new_word});
+			socket.broadcast.emit('user_won', { user: socket.id, points: game.get_scores(), word: new_word});
 		}
+	});
+	socket.on('disconnect', function () {
+		game.remove_user(socket.id);
 	});
 });
